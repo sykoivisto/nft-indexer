d9490f77b0e1b5d5428524075aa83e2d3697a00f
Sy <71138184+sykoivisto@users.noreply.github.com>
2023-05-25 09:59:30 -0400

    import bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.scss'
+import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
